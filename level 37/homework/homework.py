c31a736e3b00df5d04ef0a1f87ff05ffada03d76
GiorgiMamukashvili <167570869+GiorgiMamukashvili@users.noreply.github.com>
2024-09-12 18:17:15 +0400

    Add files via upload

---
+++ level 37/homework/homework.py
@@ -0,0 +1,19 @@
+letters =["a" "b" "c"]
+
+letters +=["b"]
+
+print(len(letters))
+
+
+#2
+txt ='abc'
+
+txt *= 2
+print(txt)
+
+#3
+Value =['hello' ,50 ,True ,10.4 ,False]
+
+Value.append('new value')
+
+print('values')
