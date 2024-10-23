5e4352561b8ec22eaa8cc370da9cfe3b1517bb18
GiorgiMamukashvili <167570869+GiorgiMamukashvili@users.noreply.github.com>
2024-10-23 18:19:24 +0400

    Add files via upload

---
+++ level 48/homework/home.py
@@ -0,0 +1,19 @@
+nam=6, 10 > 32
+nam,=3, 3 > 9
+
+month= 1, 12
+
+
+n= 5, m= 25
+n=-5, m= 80 
+
+
+
+score= 100
+score= 90
+score= 80
+score= 70
+score= 60	
+
+humanYears= 1
+print('humanYears are whole numbers only')
